6a5b8d88a3f364fe628aa69d4b4774555a8a3d19
Andrea Ciardi <developer@andreaciardi.com>
2025-11-26 01:55:51 +0200

    Added libraries for ESP32 version

---
+++ Strike32Lib/src/Strike32Lib.h
@@ -0,0 +1,14 @@
+#ifndef STRIKE32LIB_H
+#define STRIKE32LIB_H
+
+#include "Color.h"
+#include "ColorPalette.h"
+#include "RGBLed.h"
+#include "RGBDevice.h"
+#include "Feedback.h"
+#include "Hub.h"
+#include "Target.h"
+#include "Opcodes.h"
+#include "Protocol.h"
+
+#endif
